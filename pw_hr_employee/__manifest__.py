6b4f6975d5a59ac72eb840134f28fc9a8f43e4d4
Edaha <edahas86@gmail.com>
2023-05-22 20:44:15 +0300

    fields for hr and res

---
+++ pw_hr_employee/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'HR employee extensions',
+    'author': 'Ihor Sydorchenko',
+    'website': 'https://puninwine.com/',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'views/hr_employee_view.xml',
+    ],
+}
